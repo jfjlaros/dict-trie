57215bc09ef9e907e4b3b5dd5ea80848d85c4e7a
J.F.J. Laros <J.F.J.Laros@lumc.nl>
2017-02-16 15:46:43 +0100

    Version bump.

--- dict_trie/__init__.py
+++ dict_trie/__init__.py
@@ -10,7 +10,7 @@ Licensed under the MIT license, see the LICENSE file.
 from .dict_trie import Trie
 
 
-__version_info__ = ('0', '0', '1')
+__version_info__ = ('0', '0', '2')
 
 __version__ = '.'.join(__version_info__)
 __author__ = 'LUMC, Jeroen F.J. Laros'
