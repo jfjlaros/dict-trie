62138ec60899ed29ce96e3c27f33dc9ba90a9a1a
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2020-03-17 15:02:42 +0100

    Typo.

--- tests/test_trie.py
+++ tests/test_trie.py
@@ -218,11 +218,11 @@ class TestTrie(object):
     def test_levenshtein_1_ins(self):
         assert self._trie.levenshtein('abbc', 1) == 'abc'
 
-    def test_all_hamming__2(self):
+    def test_all_hamming_2(self):
         assert set(self._trie.all_hamming_('acb', 2)) == set(
             [('abc', 2, '=XX'), ('abd', 2, '=XX')])
 
-    def test_all_levenshtein__2(self):
+    def test_all_levenshtein_2(self):
         assert set(self._trie.all_levenshtein_('acb', 2)) == set([
             ('abc', 2, '=D=I'), ('abd', 2, '=D=I'), ('abc', 2, '=XX'),
             ('abd', 2, '=XX'), ('abc', 2, '=I=D')])
